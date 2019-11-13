ae6e3485a2bc9a8ad02e1091415545e3b360032c
hsisco <hysensisco@gmail.com>
2019-11-12 16:37:34 -0800

    PrivateRoute + axiosWithAuth created

---
+++ friends/src/components/PrivateRoute.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+export const PrivateRoute = ({ component: Component, ...rest }) => {
+	const Component = props.component
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (localStorage.getItem('token')) {
+          // if token is in localstorage, render the given component
+          return <Component {...props} />;
+        } else {
+          return <Redirect to="/login" />;
+      }}}
+    />
+  );
+};
