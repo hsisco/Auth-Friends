80a02a21500e964fb8f508d8d49de0b8acaeebac
hsisco <hysensisco@gmail.com>
2020-02-16 19:42:48 -0800

    Create FriendsList.js

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import CardDeck from 'react-bootstrap/CardDeck'
+
+
+class FriendsList extends React.Component {
+  state = {
+    friends: []
+  };
+
+  componentDidMount() {
+    this.getData();
+  };
+
+  getData = () => {
+    axiosWithAuth()
+      .get("/friends")
+      .then(res => {
+        console.log("Friends data:", res);
+        this.setState({
+          friends: res.data
+        });
+      })
+      .catch(err => console.log("getData FAILED:", err))
+  };
+
+  render(){
+    return(
+      <div className="friends">
+      <CardDeck>
+        {this.state.friends.map(friend => (
+          <Friend 
+            key={friend.id} 
+            friend={friend}
+          />
+        ))}
+      </CardDeck>
+    </div>
+    )
+  };
+};
+
+export default FriendsList;
