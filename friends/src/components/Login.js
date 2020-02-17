2dea434e7c6bcbf333766455c72908a436bb6b51
hsisco <hysensisco@gmail.com>
2020-02-16 20:33:41 -0800

    Update Login.js
    Delete credentials

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -4,8 +4,8 @@ import { axiosWithAuth } from '../utils/axiosWithAuth';
 class Login extends React.Component {
   state = {
     credentials: {
-      username: 'Lambda School',
-      password: 'i<3Lambd4'
+      username: '',
+      password: ''
     }
   };
 
