c9191bfb1e89739721e411cf2ff4b3a24ac06e30
hsisco <hysensisco@gmail.com>
2020-02-16 20:49:56 -0800

    Update ProtectedRoute.js
    import Login

--- friends/src/components/ProtectedRoute.js
+++ friends/src/components/ProtectedRoute.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import Login from './Login'
 
 const ProtectedRoute = ({ component: Component, ...props }) => {
   return (
