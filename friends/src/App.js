f94b91480704e28842b40786b79b2c7c379ea9b2
hsisco <hysensisco@gmail.com>
2020-02-18 19:32:43 -0800

    Update App.js
    import and use NewFriendForm

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Route, NavLink, Switch } from 'react-router-dom';
 import './App.css';
 
 import FriendsList from './components/FriendsList';
+import NewFriendForm from './components/NewFriendForm';
 import Login from './components/Login';
 import ProtectedRoute from './components/ProtectedRoute';
 
@@ -13,10 +14,10 @@ function App() {
         <NavLink to="/login">Login</NavLink>
         <NavLink to="/protected">Your Friends</NavLink>
       </header>
-
+      <NewFriendForm />
       <Switch>
         <ProtectedRoute exact path="/protected" component={FriendsList} />
-        <Route path="/login" component={Login} />
+        <Route exact path="/login" component={Login} />
       </Switch>
     </div>
   );
